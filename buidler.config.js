4ffc72fd7fac2d689bafbe0d18f37907e830519d
Pseudonaut <jquarnstrom@yahoo.com>
2020-10-19 18:17:57 -0700

    Handlebars Template, solidity-docgen

--- buidler.config.js
+++ buidler.config.js
@@ -17,6 +17,6 @@ task("accounts", "Prints the list of accounts", async () => {
 module.exports = {
   // This is a sample solc configuration that specifies which version of solc to use
   solc: {
-    version: "0.6.8",
+    version: "0.7.2",
   },
 };
