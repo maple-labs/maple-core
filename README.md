b7de699fd02738e65e8723d1ce3c74b86db999bd
Chad Lynch <chyad@hotmail.com>
2020-10-23 18:32:49 +0800

    updated repo to use hardhat, 0.7.3

--- README.md
+++ README.md
@@ -1,15 +1,34 @@
-# Maple Core
+# Maple Finance Core
 
-The Maple Finance contracts protocol.
+The Maple Core protocol.
 
-## Setup
+### Toolset
+
+- <a href="https://hardhat.org/">Hardhat</a>
+- <a href="https://solidity.readthedocs.io/en/v0.7.3/style-guide.html">Solidity 0.7.3</a>
+- <a href="https://github.com/OpenZeppelin/solidity-docgen">Solidity-Docgen</a>
+- Node v14
+- Yarn
+- VSCode
+  - Juan Blanco's Solidity plugin for VsCode
+  - Prettier Plugin
+
+### Dev Setup
 
 ```
-yarn
+npm i -g solc@0.7.3
+git clone git@github.com/maple-labs/maple-core
+cd maple-core
+
+yarn 
+yarn compile
+yarn publish
+yarn deploy
+yarn docs 
 ```
 
-## Generating Docs
+### Join us on Discord
 
-```
-yarn docs
-```
+<a href="https://discord.gg/tuNYQse">Maple Discord</a>
+
+
---
+++ scripts/publish.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const chalk = require('chalk')
+const bre = require('hardhat')
+
+const publishDir = '../react-app/src/contracts'
+
+function publishContract(contractName) {
+  console.log(
+    'Publishing',
+    chalk.cyan(contractName),
+    'to',
+    chalk.yellow(publishDir),
+  )
+  let contract = fs
+    .readFileSync(
+      `${bre.config.paths.artifacts}/contracts/${contractName}.sol/${contractName}.json`,
+    )
+    .toString()
+  const address = fs
+    .readFileSync(`${bre.config.paths.artifacts}/${contractName}.address`)
+    .toString()
+  contract = JSON.parse(contract)
+  fs.writeFileSync(
+    `${publishDir}/${contractName}.address.js`,
+    `module.exports = "${address}";`,
+  )
+  fs.writeFileSync(
+    `${publishDir}/${contractName}.abi.js`,
+    `module.exports = ${JSON.stringify(contract.abi, null, 2)};`,
+  )
+  fs.writeFileSync(
+    `${publishDir}/${contractName}.bytecode.js`,
+    `module.exports = "${contract.bytecode}";`,
+  )
+
+  return true
+}
+
+async function main() {
+  if (!fs.existsSync(publishDir)) {
+    fs.mkdirSync(publishDir)
+  }
+  const finalContractList = []
+  fs.readdirSync(bre.config.paths.sources).forEach((file) => {
+    if (file.indexOf('.sol') >= 0) {
+      const contractName = file.replace('.sol', '')
+      // Add contract to list if publishing is successful
+      if (publishContract(contractName)) {
+        finalContractList.push(contractName)
+      }
+    }
+  })
+  fs.writeFileSync(
+    `${publishDir}/contracts.js`,
+    `module.exports = ${JSON.stringify(finalContractList)};`,
+  )
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
