e4d54cdc590d35d87f1d82a241b8e4c9ec975071
cmark0v <cmark0v@undef>
2020-10-26 17:17:06 +0900

    fix broken publish script, add test and allowance for Bpool binding

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,28 @@
 const { expect } = require("chai");
 
-describe("Greeter", function() {
-  it("Should return the new greeting once it's changed", async function() {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+const mpladdy= require('../../contracts/src/contracts/MapleToken.address.js')
+const bpooladdy = require('../../contracts/src/contracts/BPool.address.js')
+
+describe("Maple", function() {
+   it("Check balance maple token", async function() {
+   const accounts = await ethers.provider.listAccounts();
+   const Mpl = await ethers.getContractFactory("MapleToken");
+   const mpl =  Mpl.attach(mpladdy)
+   const bal = await mpl.balanceOf(accounts[0])
+   console.log("mpl bal",bal.toString())
+   })
+  it("set allowances to balancer pool", async function() {
+      const accounts = await ethers.provider.listAccounts();
+      const Mpl = await ethers.getContractFactory("MapleToken");
+      const mpl =  Mpl.attach(mpladdy)
+      const allowmpl = await mpl.allowance(accounts[0], bpooladdy);
+      console.log("allowance mpl", allowmpl.toString() )
+      await mpl.approve(bpooladdy, '5000000000000000000000') 
+     const allowmpl2 = await mpl.allowance(accounts[0], bpooladdy);
+      console.log("allowance2 mpl", allowmpl2.toString() )
+
+  })
+
+})
+
