ad9724e3910676757c8fde41d7b3cc729f67a63d
cmark0v <cmark0v@undef>
2020-10-27 18:56:55 +0900

    more tests to include two bpools

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,9 +1,24 @@
 const { expect } = require('chai')
 
+const bcaddy = require('../../contracts/src/contracts/BCreator.address.js')
+const bcabi = require('../../contracts/src/contracts/BCreator.abi.js')
 const mpladdy = require('../../contracts/src/contracts/MapleToken.address.js')
-const bpooladdy = require('../../contracts/src/contracts/BPool.address.js')
+//const bpooladdy = require('../../contracts/src/contracts/BPool.address.js')
 
 describe('Maple', function () {
+  let bpooladdy
+  let bpooladdy2
+  before(async () => {
+    //const Bc =  await ethers.getContractFactory('BCreator')
+
+    const bc = new ethers.Contract(bcaddy, bcabi, ethers.provider.getSigner(0))
+    //const bc = Bc.attach(bcaddy)
+
+    bpooladdy = await bc.getBPoolAddress(0)
+    bpooladdy2 = await bc.getBPoolAddress(1)
+    console.log('BPOOLADDY DAI', bpooladdy, ' bpooladdy2 USDC', bpooladdy2)
+  })
+
   it('Check balance maple token', async function () {
     const accounts = await ethers.provider.listAccounts()
     const Mpl = await ethers.getContractFactory('MapleToken')
@@ -11,7 +26,7 @@ describe('Maple', function () {
     const bal = await mpl.balanceOf(accounts[0])
     console.log('mpl bal', bal.toString())
   })
-  it('set allowances to balancer pool', async function () {
+  it('set allowances to dai balancer pool', async function () {
     const accounts = await ethers.provider.listAccounts()
     const Mpl = await ethers.getContractFactory('MapleToken')
     const mpl = Mpl.attach(mpladdy)
@@ -19,6 +34,16 @@ describe('Maple', function () {
     console.log('allowance mpl', allowmpl.toString())
     await mpl.approve(bpooladdy, '50000000000000000000000')
     const allowmpl2 = await mpl.allowance(accounts[0], bpooladdy)
+    console.log('allowance2 mpl', allowmpl2.toString())
+  })
+  it('set allowances to usdc balancer pool', async function () {
+    const accounts = await ethers.provider.listAccounts()
+    const Mpl = await ethers.getContractFactory('MapleToken')
+    const mpl = Mpl.attach(mpladdy)
+    const allowmpl = await mpl.allowance(accounts[0], bpooladdy2)
+    console.log('allowance mpl', allowmpl.toString())
+    await mpl.approve(bpooladdy2, '50000000000000000000000')
+    const allowmpl2 = await mpl.allowance(accounts[0], bpooladdy2)
     console.log('allowance2 mpl', allowmpl2.toString())
   })
   it('give some coins to friends', async function () {
