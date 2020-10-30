72c34583df15a1b548da8c8d46ac428762da8107
Pseudonaut <jquarnstrom@yahoo.com>
2020-10-29 21:29:42 -0700

    two pub scripts

--- scripts/publish-solo.js
+++ scripts/publish-solo.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 const bre = require('hardhat')
 
-const directories = ['../react-app/src/contracts', '../contracts/src/contracts']
+const directories = ['./src']
 
 function publishContract(contractName, directory) {
   console.log(
