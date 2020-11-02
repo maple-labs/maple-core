6615239e1ee244f075fd2852c85966d3a938493e
cmark0v <cmark0v@undef>
2020-11-02 22:48:27 +0900

    cleanup

--- contracts/Math/CalcBPool.sol
+++ contracts/Math/CalcBPool.sol
@@ -6,12 +6,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //these functions will give us the ammount out if they cashed out
 //this would not be the same as how much money they put in as it includes slippage and fees
 library CalcBPool {
+  uint constant _ONE = 10**18;
   //we need to use this division function which does synthetic float with 10^-18 precision.
   //it is from balancer pool 
-      function bdiv(uint256 a, uint256 b,uint256 _tokenOne) internal pure returns (uint256) {
+      function bdiv(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b != 0, "ERR_DIV_ZERO");
-        uint256 c0 = a * _tokenOne;
-        require(a == 0 || c0 / a == _tokenOne, "ERR_DIV_INTERNAL"); // bmul overflow
+        uint256 c0 = a * _ONE;
+        require(a == 0 || c0 / a == _ONE, "ERR_DIV_INTERNAL"); // bmul overflow
         uint256 c1 = c0 + (b / 2);
         require(c1 >= c0, "ERR_DIV_INTERNAL"); //  badd require
         uint256 c2 = c1 / b;
@@ -20,14 +21,13 @@ library CalcBPool {
   function BPTVal(address _BPoolAddy,address _staker, address _liquidAssetContract) internal view returns (uint256) {
 
     //calculates the value of BPT in unites of _liquidAssetContract, in 'wei' (decimals) for this token
-    uint _ONE = 10**18;
     IBPool _IBPool = IBPool(_BPoolAddy);
     IERC20 _IBPoolERC20 = IERC20(_BPoolAddy);
     uint _BPTBal = _IBPoolERC20.balanceOf(_staker);
     uint _BPTtotal = _IBPoolERC20.totalSupply();
     uint _liquidAssetBal = _IBPool.getBalance(_liquidAssetContract);
     uint _liquidAssetWeight = _IBPool.getNormalizedWeight(_liquidAssetContract);
-    uint _val = SafeMath.div(bdiv(_BPTBal,_BPTtotal,_ONE)*bdiv(_liquidAssetBal,_liquidAssetWeight,_ONE),_ONE);
+    uint _val = SafeMath.div(bdiv(_BPTBal,_BPTtotal)*bdiv(_liquidAssetBal,_liquidAssetWeight),_ONE);
     //we have to divide out the extra _ONE with normal safemath
     //the two divisions must be separate, as coins that are lower decimals(like usdc) will underflow and give 0
     //due to the fact that the _liquidAssetWeight is a synthetic float from bpool, IE  x*10^18 where 0<x<1
