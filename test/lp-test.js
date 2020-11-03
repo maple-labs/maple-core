db3b50f35e660ac77e90cd309293ab6220e1f7db
cmark0v <cmark0v@undef>
2020-11-03 23:22:33 +0900

    another test

--- test/lp-test.js
+++ test/lp-test.js
@@ -50,6 +50,12 @@ describe('Liquidity Pool and respective lockers', function () {
     )
     dailpaddy = await lpfactory.getLiquidityPool(0)
     usdclpaddy = await lpfactory.getLiquidityPool(1)
+    dailp = new ethers.Contract(dailpaddy, lpabi, ethers.provider.getSigner(0))
+    usdclp = new ethers.Contract(
+      usdclpaddy,
+      lpabi,
+      ethers.provider.getSigner(0)
+    )
   })
   it('Check locker owners', async function () {
     const dailplocker = await lplockerfactory.getLocker(0)
@@ -60,7 +66,7 @@ describe('Liquidity Pool and respective lockers', function () {
     expect(usdclockerowner).to.equal(usdclpaddy)
   })
   it('Can not finalize DAI pool without stake', async function () {
-    dailp = new ethers.Contract(dailpaddy, lpabi, ethers.provider.getSigner(0))
+    /*dailp = new ethers.Contract(dailpaddy, lpabi, ethers.provider.getSigner(0))*/
     await expect(dailp.finalize()).to.be.revertedWith(
       'FDT_LP.makeStakeLocker: NOT_ENOUGH_STAKE'
     )
@@ -83,17 +89,17 @@ describe('Liquidity Pool and respective lockers', function () {
     await dailocker.stake('100000000000000000000')
   })
   it('Can finalize DAI pool with stake', async function () {
-    dailp = new ethers.Contract(dailpaddy, lpabi, ethers.provider.getSigner(0))
+    /*dailp = new ethers.Contract(dailpaddy, lpabi, ethers.provider.getSigner(0))*/
     await dailp.finalize()
     isfin = await dailp.isFinalized()
     expect(isfin.toString()).to.equal('true')
   })
   it('Can not finalize USDC pool without stake', async function () {
-    usdclp = new ethers.Contract(
+    /*usdclp = new ethers.Contract(
       usdclpaddy,
       lpabi,
       ethers.provider.getSigner(0)
-    )
+    )*/
     await expect(usdclp.finalize()).to.be.revertedWith(
       'FDT_LP.makeStakeLocker: NOT_ENOUGH_STAKE'
     )
@@ -116,13 +122,48 @@ describe('Liquidity Pool and respective lockers', function () {
     await usdclocker.stake('100000000000000000000')
   })
   it('Can finalize USDC pool with stake', async function () {
-    usdclp = new ethers.Contract(
+    /*usdclp = new ethers.Contract(
       usdclpaddy,
       lpabi,
       ethers.provider.getSigner(0)
-    )
+    )*/
     await usdclp.finalize()
     isfin = await usdclp.isFinalized()
     expect(isfin.toString()).to.equal('true')
+  })
+
+  //keep these two at bottom or do multiple times
+  it('DAI BPT bal of stakedAssetLocker is same as stakedassetlocker total token supply', async function () {
+    dailockeraddy = dailp.stakedAssetLocker()
+    const dailocker = new ethers.Contract(
+      dailockeraddy,
+      stakelockerabi,
+      ethers.provider.getSigner(0)
+    )
+    const daibpool = new ethers.Contract(
+      daibpooladdy,
+      bpoolabi,
+      ethers.provider.getSigner(0)
+    )
+    const totalsup = await dailocker.totalSupply()
+    const BPTbal = await daibpool.balanceOf(dailockeraddy)
+    expect(BPTbal).to.equal(totalsup)
+  })
+
+  it('USDC BPT bal of stakedAssetLocker is same as stakedassetlocker total token supply', async function () {
+    usdclockeraddy = usdclp.stakedAssetLocker()
+    const usdclocker = new ethers.Contract(
+      usdclockeraddy,
+      stakelockerabi,
+      ethers.provider.getSigner(0)
+    )
+    const usdcbpool = new ethers.Contract(
+      usdcbpooladdy,
+      bpoolabi,
+      ethers.provider.getSigner(0)
+    )
+    const totalsup = await usdclocker.totalSupply()
+    const BPTbal = await usdcbpool.balanceOf(usdclockeraddy)
+    expect(BPTbal).to.equal(totalsup)
   })
 })
