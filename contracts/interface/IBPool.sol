0a429120742e64d366f1babe234f3fdf4dbe9d91
cmark0v <cmark0v@undef>
2020-11-06 10:38:55 +0900

    ok, linted

--- contracts/interface/IBPool.sol
+++ contracts/interface/IBPool.sol
@@ -1,9 +1,13 @@
 pragma solidity 0.7.0;
 
 interface IBPool {
-    function isFinalized() external view returns (bool);
-    function isBound(address) external view returns (bool);
-    function getNumTokens() external view returns (uint256);
-    function getBalance(address)   external view  returns (uint256);
-    function getNormalizedWeight(address) external view returns (uint256);
+	function isFinalized() external view returns (bool);
+
+	function isBound(address) external view returns (bool);
+
+	function getNumTokens() external view returns (uint256);
+
+	function getBalance(address) external view returns (uint256);
+
+	function getNormalizedWeight(address) external view returns (uint256);
 }
