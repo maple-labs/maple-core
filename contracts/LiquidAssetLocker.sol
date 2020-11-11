584cc974a12dc4c28b034eeaa807697826d16010
cmark0v <cmark0v@undef>
2020-11-11 13:22:08 +0900

    fix a few things

--- contracts/LiquidAssetLocker.sol
+++ contracts/LiquidAssetLocker.sol
@@ -2,16 +2,20 @@ pragma solidity 0.7.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract LiquidAssetLocker {
+
+	//address to ERC20 contract for liquidAsset
 	address public liquidAsset;
+
+	// parent LP address for authorization purposes
 	address public immutable ownerLP;
-	constructor(address _liquidAsset, _LPaddy) public {
+
+	constructor(address _liquidAsset, address _LPaddy) public {
 		liquidAsset = _liquidAsset;
 		ownerLP = _LPaddy;
 	}
 
-	modifier isOwner(address _addy){
+	modifier isOwner(address _addy) {
 		require(_addy == ownerLP);
 		_;
 	}
-
 }
