f035308a8dcc198226b69f1f0570739446a5d9d5
Pseudonaut <jquarnstrom@yahoo.com>
2020-11-17 19:08:01 -0700

    erc-2222 core functionality

--- contracts/LoanVault.sol
+++ contracts/LoanVault.sol
@@ -55,7 +55,7 @@ contract LoanVault is IFundsDistributionToken, FundsDistributionToken {
         string memory symbol,
         address _mapleGlobals
     ) FundsDistributionToken(name, symbol) {
-        
+
         require(
             address(_assetRequested) != address(0),
             "FDT_ERC20Extension: INVALID_FUNDS_TOKEN_ADDRESS"
@@ -67,6 +67,46 @@ contract LoanVault is IFundsDistributionToken, FundsDistributionToken {
         fundsToken = IRequestedAsset;
         MapleGlobals = IGlobals(_mapleGlobals);
         borrower = tx.origin;
+    }
+
+    /**
+     * @notice Withdraws all available funds for a token holder
+     */
+    function withdrawFunds() external override {
+        uint256 withdrawableFunds = _prepareWithdraw();
+
+        require(
+            fundsToken.transfer(msg.sender, withdrawableFunds),
+            "FDT_ERC20Extension.withdrawFunds: TRANSFER_FAILED"
+        );
+
+        _updateFundsTokenBalance();
+    }
+
+    /**
+     * @dev Updates the current funds token balance
+     * and returns the difference of new and previous funds token balances
+     * @return A int256 representing the difference of the new and previous funds token balance
+     */
+    function _updateFundsTokenBalance() internal returns (int256) {
+        uint256 _prevFundsTokenBalance = fundsTokenBalance;
+
+        fundsTokenBalance = fundsToken.balanceOf(address(this));
+
+        return int256(fundsTokenBalance).sub(int256(_prevFundsTokenBalance));
+    }
+
+    /**
+     * @notice Register a payment of funds in tokens. May be called directly after a deposit is made.
+     * @dev Calls _updateFundsTokenBalance(), whereby the contract computes the delta of the previous and the new
+     * funds token balance and increments the total received funds (cumulative) by delta by calling _registerFunds()
+     */
+    function updateFundsReceived() external {
+        int256 newFunds = _updateFundsTokenBalance();
+
+        if (newFunds > 0) {
+            _distributeFunds(newFunds.toUint256Safe());
+        }
     }
 
 }
