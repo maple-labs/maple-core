2db0c3dc4a675efd179f5c1e7cbbdfc6510a566c
Chad Lynch <chyad@hotmail.com>
2020-11-20 12:41:46 +0800

    remove all scripts

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,13 +1,13 @@
-const { publish } = require('@maplelabs/hardhat-scripts')
+const { publish } = require("@maplelabs/hardhat-scripts");
 
 async function main() {
-  const directories = ['../contracts/src/contracts']
-  publish(directories)
+  const directories = ["../contracts/src/contracts"];
+  publish(directories);
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error)
-    process.exit(1)
-  })
+    console.error(error);
+    process.exit(1);
+  });
