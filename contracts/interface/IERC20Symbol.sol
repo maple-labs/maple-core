d7335ae0b8a9331f6852a4cb649b4bc885df93c8
Pseudonaut <jquarnstrom@yahoo.com>
2020-11-30 12:58:23 -0700

    symbols/address of ValidAsset simple view endpoint

---
+++ contracts/interface/IERC20Symbol.sol
@@ -0,0 +1,5 @@
+pragma solidity 0.7.0;
+
+interface IERC20Symbol {
+    function symbol() external view returns (string memory);
+}
--- test/borrower-journey.js
+++ test/borrower-journey.js
@@ -15,46 +15,15 @@ describe("Borrower Journey", function () {
       ethers.provider.getSigner(0)
     );
 
-    const BorrowTokenList_0 = await MapleGlobals.validBorrowTokenAddresses(0);
-    const BorrowTokenList_1 = await MapleGlobals.validBorrowTokenAddresses(1);
-    const BorrowTokenList_2 = await MapleGlobals.validBorrowTokenAddresses(2);
-    const BorrowTokenList_3 = await MapleGlobals.validBorrowTokenAddresses(3);
+    const List = await MapleGlobals.getValidTokens();
 
-    let tokenList = {
-      'DAI': BorrowTokenList_0,
-      'USDC': BorrowTokenList_1,
-      'WETH': BorrowTokenList_2,
-      'wBTC': BorrowTokenList_3,
-    }
-
-    // or
-
-    let tokenListPlus = {
-      'DAI':{'address': BorrowTokenList_0, 'decimals': 18},
-      'USDC':{'address': BorrowTokenList_1, 'decimals': 6},
-      'WETH':{'address': BorrowTokenList_2, 'decimals': 18},
-      'wBTC':{'address': BorrowTokenList_3, 'decimals': 8},
-    }
-
-    for (var key in tokenList) {
-        if (tokenList.hasOwnProperty(key)) {
-            console.log(key + " -> " + tokenList[key]);
-        }
-    }
-
-    for (var key in tokenListPlus) {
-        if (tokenListPlus.hasOwnProperty(key)) {
-            console.log(key + " -> " + tokenListPlus[key]['address'] + ', ' + tokenListPlus[key]['decimals']);
-        }
-    }
-
-    // const menu = (
-    //   <Menu>
-    //     <Menu.Item key="key">
-    //       tokenList[key]
-    //     </Menu.Item>
-    //   </Menu>
-    // );
+    // These two arrays are related, in order.
+    console.log(List["_validBorrowTokenSymbols"]);
+    console.log(List["_validBorrowTokenAddresses"]);
+    
+    // These two arrays are related, in order.
+    console.log(List["_validCollateralTokenSymbols"]);
+    console.log(List["_validCollateralTokenAddresses"]);
 
   });
 
