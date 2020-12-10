d060af8c71749358b4f2767285ad1f67cae6f156
Pseudonaut <jquarnstrom@yahoo.com>
2020-12-10 13:48:16 -0700

    rebase

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../../../.env" });
+require("dotenv").config({ path: "../../.env" });
 require("@nomiclabs/hardhat-waffle");
 //require('hardhat-gas-reporter');
 require("solidity-coverage");
