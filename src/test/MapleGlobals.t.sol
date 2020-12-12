1cc3458c8aeb4d2ca10f19293611a59f6af46fb1
Lucas <lucas@makerdao.com>
2020-12-12 12:48:19 -0500

    feat: set up basic testing

---
+++ src/test/MapleGlobals.t.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.6.11;
+pragma experimental ABIEncoderV2;
+
+import "ds-test/test.sol";
+import "lib/openzeppelin-contracts/src/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/src/token/ERC20/IERC20.sol";
+import "../MapleToken.sol";
+import "../MapleGlobals.sol";
+
+interface Hevm {
+    function warp(uint256) external;
+    function store(address,bytes32,bytes32) external;
+}
+
+contract MapleGlobalsTest is DSTest {
+
+    address constant DAI  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    ERC20        fundsToken;
+    MapleToken   mapleToken;
+    MapleGlobals globals;
+
+    function setUp() public {
+        fundsToken = new ERC20("FundsToken", "FT");
+        mapleToken = new MapleToken("MapleToken", "MAPL", IERC20(fundsToken));
+        globals    = new MapleGlobals(address(this), address(mapleToken));
+    }
+
+    function test_setters() public {
+        assertEq(globals.establishmentFeeBasisPoints(), 200);
+        globals.setEstablishmentFee(50);
+        assertEq(globals.establishmentFeeBasisPoints(), 50);
+
+        assertEq(globals.treasuryFeeBasisPoints(), 20);
+        globals.setTreasurySplit(30);
+        assertEq(globals.treasuryFeeBasisPoints(), 30);
+
+        assertEq(globals.gracePeriod(), 5 days);
+        globals.setGracePeriod(1 days);
+        assertEq(globals.gracePeriod(), 1 days);
+
+        assertEq(globals.stakeAmountRequired(), 25000);
+        globals.setStakeRequired(35000);
+        assertEq(globals.stakeAmountRequired(), 35000);
+
+        assertEq(globals.unstakeDelay(), 90 days);
+        globals.setUnstakeDelay(30 days);
+        assertEq(globals.unstakeDelay(), 30 days);
+
+        assertEq(globals.governor(), address(this));
+        globals.setGovernor(address(this));
+        assertEq(globals.governor(), address(this));
+    }
+
+    function test_add_tokens() public {
+        string[]  memory validBorrowTokenSymbols;
+        address[] memory validBorrowTokenAddresses;
+        string[]  memory validCollateralTokenSymbols;
+        address[] memory validCollateralTokenAddresses;
+        (
+            validBorrowTokenSymbols,
+            validBorrowTokenAddresses,
+            validCollateralTokenSymbols,
+            validCollateralTokenAddresses
+        ) = globals.getValidTokens();
+
+        assertEq(validBorrowTokenSymbols.length,       0);
+        assertEq(validBorrowTokenAddresses.length,     0);
+        assertEq(validCollateralTokenSymbols.length,   0);
+        assertEq(validCollateralTokenAddresses.length, 0);
+
+        globals.addCollateralToken(WETH);
+        (
+            validBorrowTokenSymbols,
+            validBorrowTokenAddresses,
+            validCollateralTokenSymbols,
+            validCollateralTokenAddresses
+        ) = globals.getValidTokens();
+
+        assertEq(validBorrowTokenSymbols.length,            0);
+        assertEq(validBorrowTokenAddresses.length,          0);
+        assertEq(validCollateralTokenSymbols.length,        1);
+        assertEq(validCollateralTokenAddresses.length,      1);
+        assertEq(validCollateralTokenSymbols[0],       "WETH");
+        assertEq(validCollateralTokenAddresses[0],      WETH);
+
+        globals.addBorrowToken(DAI);
+        (
+            validBorrowTokenSymbols,
+            validBorrowTokenAddresses,
+            validCollateralTokenSymbols,
+            validCollateralTokenAddresses
+        ) = globals.getValidTokens();
+
+        assertEq(validBorrowTokenSymbols.length,           1);
+        assertEq(validBorrowTokenAddresses.length,         1);
+        assertEq(validCollateralTokenSymbols.length,       1);
+        assertEq(validCollateralTokenAddresses.length,     1);
+        assertEq(validBorrowTokenSymbols[0],           "DAI");
+        assertEq(validBorrowTokenAddresses[0],          DAI);
+    }
+}
+
+
---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+[[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
+
+SOLC_FLAGS="--optimize --optimize-runs 1" dapp --use solc:0.6.11 build
+
+export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
+export DAPP_TEST_NUMBER=$(seth block latest number)
+
+# LANG=C.UTF-8 hevm dapp-test --match "quick" --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+LANG=C.UTF-8 hevm dapp-test --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
