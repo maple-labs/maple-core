875f94a81217cc6e884476db94d3c7b5b1307b27
Lucas <lucas@makerdao.com>
2020-12-13 11:18:53 -0500

    feat: add loan vault testing

--- src/test/MapleGlobals.t.sol
+++ src/test/MapleGlobals.t.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.6.11;
 pragma experimental ABIEncoderV2;
 
 import "ds-test/test.sol";
+
 import "lib/openzeppelin-contracts/src/token/ERC20/ERC20.sol";
 import "lib/openzeppelin-contracts/src/token/ERC20/IERC20.sol";
+
 import "../MapleToken.sol";
 import "../MapleGlobals.sol";
 
