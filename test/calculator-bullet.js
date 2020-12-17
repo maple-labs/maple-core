6b3f11e9d5cfc1d6fb479e3630b34e6df464a0f7
Pseudonaut <jquarnstrom@yahoo.com>
2020-12-17 15:09:06 -0700

    iterate payment test, weird error

--- test/calculator-bullet.js
+++ test/calculator-bullet.js
@@ -136,7 +136,7 @@ describe("Calculator - Bullet Repayment", function () {
 
   });
 
-  it("C - Test calculator for first repayment", async function () {
+  it("C - Iterate through payments", async function () {
 
     LoanVault = new ethers.Contract(
       vaultAddress,
@@ -144,14 +144,148 @@ describe("Calculator - Bullet Repayment", function () {
       ethers.provider.getSigner(0)
     );
 
-    const PAYMENT_INFO = await LoanVault.getNextPayment();
+    PAYMENT_INFO = await LoanVault.getNextPayment();
 
-    // console.log(parseInt(PAYMENT_INFO[0]["_hex"])); // Total
-    // console.log(parseInt(PAYMENT_INFO[1]["_hex"])); // Interest
-    // console.log(parseInt(PAYMENT_INFO[2]["_hex"])); // Principal
-    // console.log(parseInt(PAYMENT_INFO[3]["_hex"])); // Due By
+    /**
+     * parseInt(PAYMENT_INFO[0]["_hex"]) = TOTAL
+     * parseInt(PAYMENT_INFO[1]["_hex"]) = Interest
+     * parseInt(PAYMENT_INFO[2]["_hex"]) = Principal
+     * parseInt(PAYMENT_INFO[3]["_hex"]) = Due By
+    */
+    
+    DAI = new ethers.Contract(
+      DAIAddress,
+      DAIABI,
+      ethers.provider.getSigner(0)
+    );
+    
+    await DAI.approve(vaultAddress, PAYMENT_INFO[0]);
+    await LoanVault.makePayment();
 
-    expect(true);
+    PAYMENTS_REMAINING = await LoanVault.numberOfPayments();
+    PAYMENTS_REMAINING = parseInt(PAYMENTS_REMAINING["_hex"])
+    
+    while (PAYMENTS_REMAINING > 0) {
+      PAYMENT_INFO = await LoanVault.getNextPayment();
+      await DAI.approve(vaultAddress, PAYMENT_INFO[0]);
+      await LoanVault.makePayment();
+      PAYMENTS_REMAINING = await LoanVault.numberOfPayments();
+      PAYMENTS_REMAINING = parseInt(PAYMENTS_REMAINING["_hex"])
+    }
+
+    PAYMENTS_REMAINING = await LoanVault.numberOfPayments();
+    PRINCIPAL_OWED = await LoanVault.principalOwed();
+    PAYMENTS_REMAINING = parseInt(PAYMENTS_REMAINING["_hex"])
+    PRINCIPAL_OWED = parseInt(PRINCIPAL_OWED["_hex"])
+
+    expect(PAYMENTS_REMAINING).to.equals(0);
+    expect(PRINCIPAL_OWED).to.equals(0);
+
+  });
+
+  it("D - Test calculator for non 18-decimal precision, USDC(6)", async function () {
+
+    // TODO: Identify the error raised in this test.
+    
+    const LoanVaultFactoryAddress = require(artpath + "addresses/LoanVaultFactory.address");
+    const LoanVaultFactoryABI = require(artpath + "abis/LoanVaultFactory.abi");
+
+    LoanVaultFactory = new ethers.Contract(
+      LoanVaultFactoryAddress,
+      LoanVaultFactoryABI,
+      ethers.provider.getSigner(0)
+    );
+
+    const preIncrementorValue = await LoanVaultFactory.loanVaultsCreated();
+
+    // ERC-20 contracts for tokens
+    const DAIAddress = require(artpath + "addresses/MintableTokenDAI.address");
+    const USDCAddress = require(artpath + "addresses/MintableTokenUSDC.address");
+    const WETHAddress = require(artpath + "addresses/WETH9.address");
+    const WBTCAddress = require(artpath + "addresses/WBTC.address");
+    const ERC20ABI = require(artpath + "abis/MintableTokenDAI.abi");
+
+    const REQUESTED_ASSET = USDCAddress; // Update symbol variable below when changing this.
+    requestedAssetSymbol = 'USDC';
+
+    const COLLATERAL_ASSET = WETHAddress; // Update symbol variable below when changing this.
+    collateralAssetSymbol = 'WETH';
+
+    const INTEREST_STRUCTURE = 'BULLET' // 'BULLET' or 'AMORTIZATION'
+
+    const APR_BIPS = 1250; // 12.5%
+    const TERM_DAYS = 180;
+    const PAYMENT_INTERVAL_DAYS = 30;
+    const ABSTRACT_AMOUNT_MIN_RAISE = 50000; // Raising 50k USDC
+    abstractMinRaise = ABSTRACT_AMOUNT_MIN_RAISE;
+    const MIN_RAISE = BigNumber.from(
+      10 // Base 10
+    ).pow(
+      6 // Decimial precision of REQUEST_ASSET (DAI = 18, USDC = 6)
+    ).mul(
+      ABSTRACT_AMOUNT_MIN_RAISE
+    );
+    const COLLATERAL_BIPS_RATIO = 2000; // 20%
+    const FUNDING_PERIOD_DAYS = 7;
+
+    await LoanVaultFactory.createLoanVault(
+      REQUESTED_ASSET,
+      COLLATERAL_ASSET,
+      [
+        APR_BIPS, 
+        TERM_DAYS, 
+        PAYMENT_INTERVAL_DAYS, 
+        MIN_RAISE, 
+        COLLATERAL_BIPS_RATIO, 
+        FUNDING_PERIOD_DAYS
+      ],
+      ethers.utils.formatBytes32String(INTEREST_STRUCTURE),
+      {gasLimit: 6000000}
+    );
+
+    vaultAddress = await LoanVaultFactory.getLoanVault(preIncrementorValue);
+
+    USDC_EXT_1 = new ethers.Contract(USDCAddress, USDCABI, ethers.provider.getSigner(1));
+    await USDC_EXT_1.mintSpecial(accounts[1], ABSTRACT_AMOUNT_MIN_RAISE)
+    await USDC_EXT_1.approve(vaultAddress, MIN_RAISE)
+
+    LoanVault = new ethers.Contract(
+      vaultAddress,
+      LoanVaultABI,
+      ethers.provider.getSigner(1) // getSigner(1) == Lender
+    );
+
+    // Fund loan for MIN_RAISE
+    await LoanVault.fundLoan(
+      MIN_RAISE,
+      accounts[1]
+    )
+
+    LoanVault = new ethers.Contract(
+      vaultAddress,
+      LoanVaultABI,
+      ethers.provider.getSigner(0) // getSigner(0) == Borrower
+    );
+
+    // Fetch collateral amount required and approve loan vault.
+    const MIN_RAISE_ONCHAIN = await LoanVault.minRaise();
+    const COLLATERAL_REQUIRED = await LoanVault.collateralRequiredForDrawdown(MIN_RAISE_ONCHAIN);
+    
+    WBTC = new ethers.Contract(
+      WBTCAddress,
+      WBTCABI,
+      ethers.provider.getSigner(0)
+    );
+
+    await WBTC.approve(
+      vaultAddress,
+      BigNumber.from(10).pow(8).mul(Math.round(parseInt(COLLATERAL_REQUIRED["_hex"]) / 10**4)).mul(10000)
+    )
+
+    // Drawdown for the MIN_RAISE, pow(6) is USDC decimal precision
+    await LoanVault.drawdown(
+      MIN_RAISE
+    );
 
   });
 
