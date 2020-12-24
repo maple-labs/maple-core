17cc78c6a7e87a84a17fc5d38d1de2d2f23e44ad
Lucas <lucas@makerdao.com>
2020-12-24 13:55:43 -0500

    feat: remove xits

--- test/calculator-bullet.js
+++ test/calculator-bullet.js
@@ -29,7 +29,7 @@ describe("Calculator - Bullet Repayment", function () {
   let vaultAddress, abstractMinRaise;
   let collateralAssetSymbol, requestedAssetSymbol;
 
-  xit("A - Issue and fund a bullet loan", async function () {
+  it("A - Issue and fund a bullet loan", async function () {
 
     const LoanVaultFactoryAddress = require(artpath + "addresses/LoanVaultFactory.address");
     const LoanVaultFactoryABI = require(artpath + "abis/LoanVaultFactory.abi");
@@ -121,7 +121,7 @@ describe("Calculator - Bullet Repayment", function () {
 
   });
 
-  xit("B - Borrower draws down the loan", async function () {
+  it("B - Borrower draws down the loan", async function () {
 
     LoanVault = new ethers.Contract(
       vaultAddress,
@@ -151,7 +151,7 @@ describe("Calculator - Bullet Repayment", function () {
 
   });
 
-  xit("C - Iterate through payments", async function () {
+  it("C - Iterate through payments", async function () {
 
     LoanVault = new ethers.Contract(
       vaultAddress,
@@ -307,8 +307,6 @@ describe("Calculator - Bullet Repayment", function () {
     await LoanVault.drawdown(
       MIN_RAISE
     );
-    
-    // TODO: Create tests for first payment and full payment.
 
     // Make first payment.
     USDC = new ethers.Contract(
