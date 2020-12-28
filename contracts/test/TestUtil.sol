6983d595f3c90e485dc2c7aa412825359d076325
Lucas <lucas@makerdao.com>
2020-12-28 15:09:01 -0500

    feat: add TestUtil file

---
+++ contracts/test/TestUtil.sol
@@ -0,0 +1,56 @@
+pragma solidity >=0.6.11;
+
+import "lib/ds-test/contracts/test.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+interface Hevm {
+    function warp(uint256) external;
+    function store(address,bytes32,bytes32) external;
+}
+
+contract TestUtil is DSTest {
+    Hevm hevm;
+
+    struct Token {
+        address addr; // ERC20 Mainnet address
+        uint256 slot; // Balance storage slot
+    }
+
+    mapping (bytes32 => Token) tokens;
+
+    address constant DAI  = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    address constant BPOOL_FACTORY = 0x9424B1412450D0f8Fc2255FAf6046b98213B76Bd; // Balancer pool factory
+
+    uint256 constant WAD = 10 ** 18;
+
+    bytes20 constant CHEAT_CODE = bytes20(uint160(uint256(keccak256('hevm cheat code'))));
+
+    constructor() public {
+        hevm = Hevm(address(CHEAT_CODE));
+
+        tokens["DAI"].addr  = DAI;
+        tokens["DAI"].slot  = 2;
+        tokens["USDC"].addr = USDC;
+        tokens["USDC"].slot = 9;
+        tokens["WETH"].addr = WETH;
+        tokens["WETH"].slot = 3;
+    }
+
+    // Manipulate mainnet ERC20 balance
+    function mint(bytes32 symbol, address who, uint256 amt) public {
+        address addr = tokens[symbol].addr;
+        uint256 slot  = tokens[symbol].slot;
+        uint256 bal = IERC20(addr).balanceOf(who);
+
+        hevm.store(
+            addr,
+            keccak256(abi.encode(who, slot)), // Mint tokens
+            bytes32(amt)
+        );
+
+        assertEq(IERC20(addr).balanceOf(who), bal + amt); // Assert new balance
+    }
+}
