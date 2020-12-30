ebb7ab01dee9daf22be734ad80e9d307c96b7d0b
Lucas <lucas@makerdao.com>
2020-12-30 11:56:13 -0500

    feat: set up claim test

--- test.sh
+++ test.sh
@@ -8,6 +8,10 @@ DAPP_SRC="contracts" SOLC_FLAGS="--optimize --optimize-runs 1" dapp --use solc:0
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 
+<<<<<<< HEAD
 LANG=C.UTF-8 DAPP_SRC="contracts" hevm dapp-test --match "test_claim_singleLP" --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+=======
+LANG=C.UTF-8 DAPP_SRC="contracts" hevm dapp-test --match "test_claim" --rpc="$ETH_RPC_URL" --json-file=out/dapp.sol.json --dapp-root=. --verbose 1
+>>>>>>> 6baf3fc... feat: set up claim test
 
 # --match "test_drawdown"
