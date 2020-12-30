d01d61f53d96cdfea3884f92457b5b298b22e34b
pseudonaut <jquarnstrom@yahoo.com>
2020-12-30 14:42:08 -0700

    todo note

--- contracts/interfaces/ILoanVault.sol
+++ contracts/interfaces/ILoanVault.sol
@@ -36,6 +36,8 @@ interface ILoanVault is IERC20 {
     
     // Functions
     function fundLoan(uint256, address) external;
+
+    // TODO: Create IFDT.sol (IFundsDistributionToken, inherit here and other places)
     function updateFundsReceived() external;
     function withdrawFunds() external;
 }
