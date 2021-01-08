42f0651d3bce4571b8ccf070bf0b03dc647d09fe
pseudonaut <jquarnstrom@yahoo.com>
2021-01-07 17:09:17 -0700

    update fundingLockerFactory interface

--- contracts/interfaces/IFundingLockerFactory.sol
+++ contracts/interfaces/IFundingLockerFactory.sol
@@ -3,8 +3,4 @@ pragma solidity >=0.6.11;
 
 interface IFundingLockerFactory {
     function newLocker(address) external returns (address);
-
-    function getOwner(address) external view returns (address);
-
-    function verifyLocker(address) external view returns (bool);
 }
