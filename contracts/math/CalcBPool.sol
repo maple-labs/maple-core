cb40148f1e7062bda7014d55f1f1636f13a276d5
pseudonaut <jquarnstrom@yahoo.com>
2021-01-08 21:18:55 -0700

    nit: AmortizationRepaymentCalc.sol natspec, CalcBPool.sol WAD standardization

--- contracts/math/CalcBPool.sol
+++ contracts/math/CalcBPool.sol
@@ -5,29 +5,25 @@ import "lib/openzeppelin-contracts/contracts/math/SafeMath.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import "../interfaces/IBPool.sol";
 import "../interfaces/IGlobals.sol";
-// import "hardhat/console.sol";
-
-//we might want to do this with functions built into BPool
-//these functions will give us the ammount out if they cashed out
-//this would not be the same as how much money they put in as it includes slippage and fees
 
 contract CalcBPool {
 
     using SafeMath for uint256;
-    uint256 constant _ONE = 10**18;
+
+    uint256 constant WAD = 10**18;
 
     /// @notice Official balancer pool bdiv() function, does synthetic float with 10^-18 precision.
     function bdiv(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b != 0, "ERR_DIV_ZERO");
-        uint256 c0 = a * _ONE;
-        require(a == 0 || c0 / a == _ONE, "ERR_DIV_INTERNAL"); // bmul overflow
+        uint256 c0 = a * WAD;
+        require(a == 0 || c0 / a == WAD, "ERR_DIV_INTERNAL"); // bmul overflow
         uint256 c1 = c0 + (b / 2);
         require(c1 >= c0, "ERR_DIV_INTERNAL"); //  badd require
         uint256 c2 = c1 / b;
         return c2;
     }
 
-    /// @notice Calculates the value of BPT in units of _liquidityAssetContract, in 'wei' (decimals) for this token
+    /// @notice Calculates the value of BPT in units of _liquidityAssetContract in 'wei' (decimals) for this token.
     function BPTVal(
         address _pool,
         address _pair,
@@ -47,21 +43,23 @@ contract CalcBPool {
         uint256 totalSupplyBPT = bPoolERC20.totalSupply();
         uint256 liquidityAssetBalance = bPool.getBalance(_pair);
         uint256 liquidityAssetWeight = bPool.getNormalizedWeight(_pair);
-        uint256 _val = bdiv(amountStakedBPT, totalSupplyBPT).mul(bdiv(liquidityAssetBalance, liquidityAssetWeight)).div(_ONE);
+        uint256 _val = bdiv(amountStakedBPT, totalSupplyBPT).mul(bdiv(liquidityAssetBalance, liquidityAssetWeight)).div(WAD);
         
-        //we have to divide out the extra _ONE with normal safemath
+        //we have to divide out the extra WAD with normal safemath
         //the two divisions must be separate, as coins that are lower decimals(like usdc) will underflow and give 0
         //due to the fact that the _liquidityAssetWeight is a synthetic float from bpool, IE  x*10^18 where 0<x<1
         //the result here is
         return _val;
     }
 
-    /// @notice Calculates the USDC swap out value of the _staker BPTs held in _stakeLocker.
-    /// @param _pool is the official Maple Balancer pool.
-    /// @param _pair is the asset paired 50/50 with MPL in the official Maple Balancer pool.
-    /// @param _staker is the staker who deposited to the StakeLocker.
-    /// @param _stakeLocker is the address of the StakeLocker.
-    /// @return uint, USDC swap out value of _staker BPTs.
+    /** 
+        @notice Calculate _pair swap out value of _staker BPT balance escrowed in _stakeLocker.
+        @param _pool is the balancer pool.
+        @param _pair is the swap out asset (e.g. USDC).
+        @param _staker is the address that deposited BPTs to _stakeLocker.
+        @param _stakeLocker escrow the BPTS deposited by _staker.
+        @return uint, USDC swap out value of _staker BPTs.
+    */
     function getSwapOutValue(
         address _pool,
         address _pair,
@@ -137,6 +135,6 @@ contract CalcBPool {
         // console.log("poolAmountInRequired", poolAmountInRequired);
         // console.log("stakerBalance", stakerBalance);
 
-        return (poolAmountInRequired, stakerBalance.div(_ONE));
+        return (poolAmountInRequired, stakerBalance.div(WAD));
     }
 }
