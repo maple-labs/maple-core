90014c3a88ca283abc255fad78f6c8dc952ac3f0
pseudonaut <jquarnstrom@yahoo.com>
2021-01-10 12:08:26 -0700

    nit: spacing on mapleToken

--- contracts/MapleToken.sol
+++ contracts/MapleToken.sol
@@ -5,26 +5,26 @@ import "./token/FDT.sol";
 
 contract MapleToken is FDT {
 
-		modifier onlyFundsToken () {
-				require(
-						msg.sender == address(fundsToken), 
-						"FDT_ERC20Extension.onlyFundsToken: UNAUTHORIZED_SENDER"
-				);
-				_;
-		}
+    modifier onlyFundsToken () {
+        require(
+            msg.sender == address(fundsToken), 
+            "FDT_ERC20Extension.onlyFundsToken: UNAUTHORIZED_SENDER"
+        );
+        _;
+    }
 
-		/**
-				@notice Instanties the MapleToken.
-        @param  name   		 Name of the token.
-        @param  symbol 		 Symbol of the token.
+    /**
+        @notice Instanties the MapleToken.
+        @param  name       Name of the token.
+        @param  symbol     Symbol of the token.
         @param  fundsToken The asset claimable / distributed via ERC-2222, deposited to MapleToken contract.
     */
-		constructor (
-				string memory name, 
-				string memory symbol, 
-				address fundsToken
-		) FDT(name, symbol, fundsToken) public {
-				require(address(fundsToken) != address(0), "FDT_ERC20Extension: INVALID_FUNDS_TOKEN_ADDRESS");
-				_mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
-		}
+    constructor (
+        string memory name, 
+        string memory symbol, 
+        address fundsToken
+    ) FDT(name, symbol, fundsToken) public {
+        require(address(fundsToken) != address(0), "FDT_ERC20Extension: INVALID_FUNDS_TOKEN_ADDRESS");
+        _mint(msg.sender, 10000000 * (10 ** uint256(decimals())));
+    }
 }
