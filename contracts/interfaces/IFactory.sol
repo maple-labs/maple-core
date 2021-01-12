b0abcdc110ec2effb564be46cd9fbec1cb25bc09
cmark0v <cmark0v@undef>
2021-01-12 14:35:50 +0900

    factory inteface

---
+++ contracts/interfaces/IFactory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.11;
+
+interface IFactory {
+    function newLocker(address) external returns (address);
+    function owner(address) external returns (address);
+    function isLocker(address) external returns (bool);
+}
