f342c0265c191ad4477138972dc8db1fb974945a
Lucas Manuel <lucas@maple.finance>
2021-01-14 15:07:55 -0500

    feat: Add fuzzing tests for calculators, add scripts (SC-1128) (#141)
    * feat: add CI back, fuzz tests pass
    
    * fix: remove unit test from CI
    
    * fix: rm .DS_Stroe
    
    * feat: update tests to account for multi factory
    
    * fix: try CI fix
    
    * fix: remove unit test from CI
    
    * feat: add docs
    
    * fix: compliation issue
    
    Co-authored-by: Lucas <lucas@makerdao.com>

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ The Maple Core protocol.
 ### Toolset
 
 - <a href="https://hardhat.org/">Hardhat</a>
-- <a href="https://solidity.readthedocs.io/en/v0.7.3/style-guide.html">Solidity 0.7.3</a>
+- <a href="https://docs.soliditylang.org/en/v0.6.11/">Solidity 0.6.11</a>
 - <a href="https://github.com/OpenZeppelin/solidity-docgen">Solidity-Docgen</a>
 - Node v14
 - Yarn
@@ -16,7 +16,7 @@ The Maple Core protocol.
 ### Dev Setup
 
 ```
-npm i -g solc@0.7.3
+npm i -g solc@0.6.11
 git clone git@github.com/maple-labs/maple-core
 cd maple-core
 
@@ -25,8 +25,18 @@ yarn compile
 yarn publish
 yarn deploy
 yarn docs 
+
+dapp update
 ```
 
+### Testing
+- To run unit tests: `make unit-test`
+- To run fuzz tests: `make fuzz-test` (runs `test-fuzz.sh`)
+- To run all tests:  `make ci-test` (runs `test-ci.sh`)
+
+To alter number of fuzz runs, change the `--fuzz-runs` flag in `./test-ci.sh` or `test-fuzz.sh`
+
+Note: Number of `--fuzz-runs` in `./test-ci.sh` should remain constant on push. Only change for local testing if needed.
 ### Join us on Discord
 
 <a href="https://discord.gg/tuNYQse">Maple Discord</a>
--- contracts/LateFeeCalc.sol
+++ contracts/LateFeeCalc.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.11;
 
 import "lib/openzeppelin-contracts/contracts/math/SafeMath.sol";
 import "./interfaces/ILoan.sol";
+import "./interfaces/IRepaymentCalc.sol";
 
 /// @title LateFeeCalc applies a flat fee on the amount owed for next payment.
 contract LateFeeCalc {
@@ -20,14 +21,14 @@ contract LateFeeCalc {
 
     /**
         @dev    Calculates the late fee payment for a _loan.
-        @param  _loan is the Loan to calculate late fee for.
+        @param  loan is the Loan to calculate late fee for.
         @return [0] = Principal + Interest (Total)
                 [1] = Principal
                 [2] = Interest
     */
-    function getLateFee(address _loan) view public returns(uint256, uint256, uint256) {
-        ILoan loan           = ILoan(_loan);
-        (uint paymentDue,,,) = loan.getNextPayment();
+    function getLateFee(address loan) view public returns(uint256, uint256, uint256) {
+        IRepaymentCalc repaymentCalc = IRepaymentCalc(ILoan(loan).repaymentCalc());
+        (uint paymentDue,,)          = repaymentCalc.getNextPayment(address(loan));
         return (paymentDue.mul(feeBips).div(10000), 0, paymentDue.mul(feeBips).div(10000));
     }
 } 
