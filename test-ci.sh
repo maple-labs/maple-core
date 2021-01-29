d78a46a78df9baffd0fe8f046b75fd8539f834f1
Lucas <lucas@makerdao.com>
2021-01-29 11:01:05 -0500

    feat: use specific dapphub commit

--- test-ci.sh
+++ test-ci.sh
@@ -4,8 +4,6 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
-# DAPP_SRC="contracts" SOLC_FLAGS="--optimize --optimize-runs 200" dapp --use solc:0.6.11 build
-
 export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
 export DAPP_TEST_NUMBER=$(seth block latest number)
 
