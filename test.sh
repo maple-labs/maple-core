de9dce6cef3751007d61ccbb580a357a5daf07dd
satyam <satyam@secureblocks.io>
2021-01-30 11:26:17 +0530

    fix test script

--- test.sh
+++ test.sh
@@ -11,6 +11,6 @@ export DAPP_SRC="contracts"
 export SOLC_FLAGS="--optimize --optimize-runs 200"
 
 dapp build
-LANG=C.UTF-8 dapp test --match "test_getInitialStakeRequirements" --rpc-url "$ETH_RPC_URL" --verbose
+LANG=C.UTF-8 dapp test --match "contracts/test" --rpc-url "$ETH_RPC_URL" --verbose
 
 # --match "contracts/test/MapleGlobals.t.sol" 
