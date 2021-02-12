db8f7a750f502404579890317f9da0c6dc98a54f
Michael Elliot <michael@digital.com.au>
2021-02-12 10:02:34 +0800

    feat: add maple-cache submodule and use dapp test caching (#208)
    * feat: add maple-cache submodule and use dapp test caching
    
    * feat: add parallelism back to CI
    
    * fix: move dapp-cache submodule step to build job
    
    * fix: add dapp update back
    
    Co-authored-by: Lucas <lucas@makerdao.com>

--- test-ci.sh
+++ test-ci.sh
@@ -3,14 +3,14 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
-export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
-export DAPP_TEST_NUMBER=$(seth block latest number)	export DAPP_TEST_NUMBER=$(seth block latest number)
+export DAPP_TEST_TIMESTAMP=1612311576
+export DAPP_TEST_NUMBER=11780000
 export DAPP_SKIP_BUILD=1
 export DAPP_SOLC_VERSION=0.6.11
 export DAPP_SRC="contracts"
 export SOLC_FLAGS="--optimize --optimize-runs 200"
 export DAPP_LINK_TEST_LIBRARIES=1
 
-LANG=C.UTF-8 dapp test --match ${1} --rpc-url "$ETH_RPC_URL" --verbose
+LANG=C.UTF-8 dapp test --match ${1} --rpc-url "$ETH_RPC_URL" --verbose --cache "cache/dapp-cache"
 
 # --match "contracts/test/MapleGlobals.t.sol" 
--- test.sh
+++ test.sh
@@ -3,13 +3,13 @@ set -e
 
 [[ "$ETH_RPC_URL" && "$(seth chain)" == "ethlive"  ]] || { echo "Please set a mainnet ETH_RPC_URL"; exit 1;  }
 
-export DAPP_TEST_TIMESTAMP=$(seth block latest timestamp)
-export DAPP_TEST_NUMBER=$(seth block latest number)
+export DAPP_TEST_TIMESTAMP=1612311576
+export DAPP_TEST_NUMBER=11780000
 export DAPP_SOLC_VERSION=0.6.11
 export DAPP_SRC="contracts"
 export SOLC_FLAGS="--optimize --optimize-runs 200"
 export DAPP_LINK_TEST_LIBRARIES=1
 
-LANG=C.UTF-8 dapp test --match "contracts/test" --rpc-url "$ETH_RPC_URL" --verbose
+LANG=C.UTF-8 dapp test --match "contracts/test" --rpc-url "$ETH_RPC_URL" --verbose --cache "cache/dapp-cache"
 
 # --match "contracts/test/MapleGlobals.t.sol" 
