30bd2e7d789b7199a10bab6efe9fd60951df3ced
Satyam Agrawal <satyam0499@gmail.com>
2021-02-17 11:24:46 +0530

    minor improvements in tests

--- contracts/test/ChainLinkOracle.t.sol
+++ contracts/test/ChainLinkOracle.t.sol
@@ -43,18 +43,18 @@ contract ChainlinkOracleTest is TestUtil {
         
         // Set manual price
         assertTrue(!fakeAdmin.try_setManualOverride(address(oracle), true));
+        assertTrue(    !admin.try_setManualOverride(address(oracle), true));
+        
         assertTrue(!fakeAdmin.try_setManualPrice(address(oracle), int256(45000)));
-        // Use authorized owner.
-        admin.setManualOverride(address(oracle), true);
-        admin.setManualPrice(address(oracle), int256(45000));
-        assertTrue(oracle.manualOverride());
+        assertTrue(    !admin.try_setManualPrice(address(oracle), int256(45000)));
 
+        assertTrue(oracle.manualOverride());
         assertEq(oracle.manualPrice(),    int256(45000));
         assertEq(oracle.getLatestPrice(), int256(45000));
 
         // Change aggregator.
         assertTrue(!fakeAdmin.try_changeAggregator(address(oracle), 0xb022E2970b3501d8d83eD07912330d178543C1eB));
-        admin.changeAggregator(address(oracle), 0xb022E2970b3501d8d83eD07912330d178543C1eB);
+        assertTrue(    !admin.try_changeAggregator(address(oracle), 0xb022E2970b3501d8d83eD07912330d178543C1eB));
         assertEq(address(oracle.priceFeed()), 0xb022E2970b3501d8d83eD07912330d178543C1eB);
 
         assertTrue(oracle.getLatestPrice() != currentPrice);
