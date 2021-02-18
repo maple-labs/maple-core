aba26cf7aa0e40b74689b96ebac3bec64ab9576f
Lucas <lucas@makerdao.com>
2021-02-18 08:44:09 -0500

    feat: remove oracle TODOs, add to util

--- contracts/fuzz/BulletRepaymentCalc.t.sol
+++ contracts/fuzz/BulletRepaymentCalc.t.sol
@@ -125,9 +125,9 @@ contract BulletRepaymentCalcTest is TestUtil {
         gov.setValidSubFactory(address(poolFactory), address(dlFactory1), true);
         gov.setValidSubFactory(address(poolFactory), address(dlFactory2), true);
 
-        wethOracle = new ChainlinkOracle(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419, WETH, address(this));
-        wbtcOracle = new ChainlinkOracle(0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c, WBTC, address(this));
-        usdOracle  = new ChainlinkOracle(0xAed0c38402a5d19df6E4c03F4E2DceD6e29c1ee9, USDC, address(this));
+        wethOracle = new ChainlinkOracle(tokens["WETH"].orcl, WETH, address(this));
+        wbtcOracle = new ChainlinkOracle(tokens["WBTC"].orcl, WBTC, address(this));
+        usdOracle  = new ChainlinkOracle(tokens["DAI"].orcl, USDC, address(this));
         
         gov.setPriceOracle(WETH, address(wethOracle));
         gov.setPriceOracle(WBTC, address(wbtcOracle));
