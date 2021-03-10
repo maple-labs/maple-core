94a6f51bf8ab61673348c2fe5309b1a19444b59e
Lucas <lucas@maple.finance>
2021-03-10 10:31:32 -0500

    fix: update comment

--- contracts/StakeLocker.sol
+++ contracts/StakeLocker.sol
@@ -163,7 +163,7 @@ contract StakeLocker is StakeLockerFDT, Pausable {
             stakeDate[who] = block.timestamp;
         } else {
             uint256 dTime  = block.timestamp.sub(stkDate); 
-            stakeDate[who] = stkDate.add(dTime.mul(amt).div(balanceOf(who) + amt));  // date + (now - stkDate) * coef
+            stakeDate[who] = stkDate.add(dTime.mul(amt).div(balanceOf(who) + amt));  // depDate + (now - depDate) * (amt / (balance + amt))
         }
     }
 
