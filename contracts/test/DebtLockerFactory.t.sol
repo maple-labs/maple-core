adcef5f12d013b04bbf8c8cd8c363dbc8c146c90
Satyam Agrawal <satyam0499@gmail.com>
2021-03-24 20:52:55 +0530

    chore: Add consistency in the factories (#303)
    * add consistency in the factories
    
    * fix: alignment
    
    Co-authored-by: Lucas <lucas@maple.finance>

--- contracts/test/DebtLockerFactory.t.sol
+++ contracts/test/DebtLockerFactory.t.sol
@@ -78,12 +78,12 @@ contract DebtLockerFactoryTest is TestUtil {
     function test_newLocker() public {
         DebtLocker dl  = DebtLocker(dlFactory.newLocker(address(loan)));
         // Validate the storage of dlfactory.
-        assertEq(dlFactory.owner(address(dl)), address(this));
+        assertEq(  dlFactory.owner(address(dl)), address(this));
         assertTrue(dlFactory.isLocker(address(dl)));
 
         // Validate the storage of dl.
-        assertEq(address(dl.loan()),      address(loan), "Incorrect loan address");
-        assertEq(dl.owner(),     address(this), "Incorrect owner of the DebtLocker");
-        assertEq(address(dl.loanAsset()), USDC, "Incorrect address of loan asset");
+        assertEq(address(dl.loan()),       address(loan), "Incorrect loan address");
+        assertEq(dl.pool(),                address(this), "Incorrect owner of the DebtLocker");
+        assertEq(address(dl.loanAsset()),  USDC,          "Incorrect address of loan asset");
     }
 }
