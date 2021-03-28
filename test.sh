ed067d201ece61b7ae366c1cf93e1ab27cd797fa
Lucas <lucas@maple.finance>
2021-03-28 17:35:48 -0400

    feat: add optimization to tests, use verbosity 1

--- test.sh
+++ test.sh
@@ -7,9 +7,10 @@ export DAPP_TEST_TIMESTAMP=1615792486
 export DAPP_TEST_NUMBER=12045000
 export DAPP_SOLC_VERSION=0.6.11
 export DAPP_SRC="contracts"
-export SOLC_FLAGS="--optimize --optimize-runs 200"
+export DAPP_BUILD_OPTIMIZE=1
+export DAPP_BUILD_OPTIMIZE_RUNS=200
 export DAPP_LINK_TEST_LIBRARIES=1
 
 if [ ${1} ]; then match=${1}; else match="contracts/test"; fi
 
-LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity 2 --cache "cache/dapp-cache" --fuzz-runs 10
+LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity 1 --cache "cache/dapp-cache" --fuzz-runs 10
