0cc9733ab0e4de1ce7a35a107c0df930b3710c5a
Lucas Manuel <lucas@maple.finance>
2021-03-30 10:22:35 -0400

    feat: Update LateFeeCalc to return only interest (SC-1723) (#323)
    * feat: update late fee calc to return only interest
    
    * fix: update total with late fee

--- contracts/interfaces/ILateFeeCalc.sol
+++ contracts/interfaces/ILateFeeCalc.sol
@@ -2,5 +2,5 @@
 pragma solidity 0.6.11;
 
 interface ILateFeeCalc {
-    function getLateFee(address) external view returns (uint256, uint256, uint256);
+    function getLateFee(address) external view returns (uint256);
 } 
