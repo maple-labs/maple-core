629e86da604941fb0c4d725b17e425068557a6ae
Michael Elliot <michael@digital.com.au>
2021-04-07 12:36:26 +0800

    chore: optimize flatten script

--- flatten.sh
+++ flatten.sh
@@ -1,11 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+mkdir -p flatten/functions
+
 FILES=./contracts/*.sol
 for f in $FILES
 do
-  # take action on each file. $f store current file name
-#   hevm flatten --source-file "contracts/StakeLocker.sol" > flatten/StakeLocker.sol
     filepath=${f:2}
     filename=${filepath:10}
     echo "Flattening: $filename"
     hevm flatten --source-file $filepath > "flatten/$filename"
-    hevm flatten --source-file $filepath | grep -E ") external |) public" > "flatten/functions/$filename"
+    cat "flatten/$filename" | grep -E ") external |) public" > "flatten/functions/$filename"
 done
