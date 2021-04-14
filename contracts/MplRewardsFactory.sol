5438542c61c208c46e9bcf133e457183cbd33566
Michael Elliot <michael@digital.com.au>
2021-04-14 20:17:17 +0800

    index mplRewards param in MplRewardsCreated event

--- contracts/MplRewardsFactory.sol
+++ contracts/MplRewardsFactory.sol
@@ -11,7 +11,7 @@ contract MplRewardsFactory {
 
     mapping(address => bool) public isMplRewards;  // True if MplRewards was created by this factory, otherwise false.
 
-    event MplRewardsCreated(address indexed rewardsToken, address indexed stakingToken, address mplRewards, address owner);
+    event MplRewardsCreated(address indexed rewardsToken, address indexed stakingToken, address indexed mplRewards, address owner);
 
     constructor(address _globals) public {
         globals = IGlobals(_globals);
