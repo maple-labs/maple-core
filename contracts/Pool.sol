53878436f0d194b69eda2385d5fd790a6c397d41
Michael De Luca <michael.deluca@circle-free.com>
2021-04-26 17:56:44 -0400

    chore: PR Review fixes
    - Revert saving context variable locally
    - Fix allignment

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -370,17 +370,16 @@ contract Pool is PoolFDT {
         _isValidState(State.Finalized);
         require(isDepositAllowed(amt), "P:DEPOSIT_NOT_ALLOWED");
 
-        address msgSender           = msg.sender;
-        withdrawCooldown[msgSender] = uint256(0);  // Reset withdrawCooldown if LP had previously intended to withdraw
+        withdrawCooldown[msg.sender] = uint256(0);  // Reset withdrawCooldown if LP had previously intended to withdraw
 
         uint256 wad = _toWad(amt);
-        PoolLib.updateDepositDate(depositDate, balanceOf(msgSender), wad, msgSender);
+        PoolLib.updateDepositDate(depositDate, balanceOf(msg.sender), wad, msg.sender);
 
-        liquidityAsset.safeTransferFrom(msgSender, liquidityLocker, amt);
-        _mint(msgSender, wad);
+        liquidityAsset.safeTransferFrom(msg.sender, liquidityLocker, amt);
+        _mint(msg.sender, wad);
 
         _emitBalanceUpdatedEvent();
-        emit Cooldown(msgSender, uint256(0));
+        emit Cooldown(msg.sender, uint256(0));
     }
 
     /**
@@ -408,12 +407,12 @@ contract Pool is PoolFDT {
         require(PoolLib.isWithdrawAllowed(withdrawCooldown[msg.sender], _globals(superFactory)), "P:WITHDRAW_NOT_ALLOWED");
         require(depositDate[msg.sender].add(lockupPeriod) <= block.timestamp,                    "P:FUNDS_LOCKED");
 
-        _burn(msgSender, wad);  // Burn the corresponding FDT balance
+        _burn(msg.sender, wad);  // Burn the corresponding FDT balance
         withdrawFunds();         // Transfer full entitled interest, decrement `interestSum`
 
         // Transfer amount that is due after realized losses are accounted for.
         // recognizedLosses are absorbed by the LP.
-        _transferLiquidityLockerFunds(msgSender, amt.sub(recognizeLosses()));
+        _transferLiquidityLockerFunds(msg.sender, amt.sub(recognizeLosses()));
 
         _emitBalanceUpdatedEvent();
     }
