1f41cb30c3c6b0c1aadee7317a75c4d625bf5e33
Michael De Luca <35537333+deluca-mike@users.noreply.github.com>
2021-05-17 16:56:58 -0400

    feat: Committed Flattened Contracts (#402)
    Co-authored-by: Lucas Manuel <lucas@maple.finance>

--- flatten.sh
+++ flatten.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-mkdir -p flatten/functions
+mkdir -p flattened-contracts/functions
 
 FILES1=./contracts/*.sol
 FILES2=./contracts/library/*.sol
+FILES3=./contracts/oracles/*.sol
+
 for f in $FILES1
 do
     filepath=${f:2}
     filename=${filepath:10}
     echo "Flattening: $filename"
-    hevm flatten --source-file $filepath | sed '/SPDX-License-Identifier/d' | sed -e '1s/^/ \/\/ SPDX-License-Identifier:  AGPL-3.0-or-later /' > "flatten/$filename"
-    cat "flatten/$filename" | grep -E ") external |) public" > "flatten/functions/$filename"
+    hevm flatten --source-file $filepath | sed '/SPDX-License-Identifier/d' | sed -e '1s/^/\/\/ SPDX-License-Identifier:  AGPL-3.0-or-later /' | sed -r 'N;s/(\/\/\/\/\/\/ .*)\n\s*$/\1/;P;D' > "flattened-contracts/$filename"
+    cat "flattened-contracts/$filename" | grep -E ") external |) public" > "flattened-contracts/functions/$filename"
 done
 
 for f in $FILES2
@@ -19,5 +21,13 @@ do
     filepath=${f:2}
     filename=${filepath:17}
     echo "Flattening: $filename"
-    hevm flatten --source-file $filepath | sed '/SPDX-License-Identifier/d' | sed -e '1s/^/ \/\/ SPDX-License-Identifier:  AGPL-3.0-or-later /' > "flatten/$filename"
+    hevm flatten --source-file $filepath | sed '/SPDX-License-Identifier/d' | sed -e '1s/^/\/\/ SPDX-License-Identifier:  AGPL-3.0-or-later /' | sed -r 'N;s/(\/\/\/\/\/\/ .*)\n\s*$/\1/;P;D' > "flattened-contracts/$filename"
+done
+
+for f in $FILES3
+do
+    filepath=${f:2}
+    filename=${filepath:17}
+    echo "Flattening: $filename"
+    hevm flatten --source-file $filepath | sed '/SPDX-License-Identifier/d' | sed -e '1s/^/\/\/ SPDX-License-Identifier:  AGPL-3.0-or-later /' | sed -r 'N;s/(\/\/\/\/\/\/ .*)\n\s*$/\1/;P;D' > "flattened-contracts/$filename"
 done
