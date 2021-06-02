9333e1aae8c66d744d0e3c94c0a7eac71773e086
Lucas <lucas@maple.finance>
2021-06-02 14:30:21 -0400

    feat: update ci and build script to use libraries and remapings

--- test.sh
+++ test.sh
@@ -25,9 +25,7 @@ export DAPP_TEST_NUMBER=12543537
 export DAPP_SOLC_VERSION=0.6.11
 export DAPP_SRC="contracts"
 export DAPP_LINK_TEST_LIBRARIES=0
-export DAPP_STANDARD_JSON="config.json"
-# export DAPP_REMAPPINGS=$(cat remappings)
-# export DAPP_LIBRARIES=" contracts/libraries/loan/v1/LoanLib.sol:LoanLib:0x51A189ccD2eB5e1168DdcA7e59F7c8f39AA52232 contracts/libraries/pool/v1/PoolLib.sol:PoolLib:0x2c1C30fb8cC313Ef3cfd2E2bBf2da88AdD902C30 contracts/libraries/util/v1/Util.sol:Util:0x95f9676A34aF2675B63948dDba8F8c798741A52a"
+export DAPP_STANDARD_JSON=$config
 
 if [ "$skip_build" = "1" ]; then export DAPP_SKIP_BUILD=1; fi
 
