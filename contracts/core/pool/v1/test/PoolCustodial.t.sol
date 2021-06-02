489383b81f2865a966917bf9d1b5c2edecde5214
Lucas <lucas@maple.finance>
2021-06-02 11:51:53 -0400

    feat: add test remapping

--- contracts/core/pool/v1/test/PoolCustodial.t.sol
+++ contracts/core/pool/v1/test/PoolCustodial.t.sol
@@ -2,9 +2,9 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import "../../../../test/TestUtil.sol";
+import "test/TestUtil.sol";
 
-import "../../../../test/user/Custodian.sol";
+import "test/user/Custodian.sol";
 
 contract PoolCustodialTest is TestUtil {
 
