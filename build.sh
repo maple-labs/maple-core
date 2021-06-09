8943b8cb3d7c2a28da9aed08782c45d7387ba74d
Michael De Luca <michael.deluca@circle-free.com>
2021-06-09 18:15:35 -0400

    Fix: PR Review

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ export DAPP_TEST_NUMBER=12543537
 export DAPP_SOLC_VERSION=0.6.11
 export DAPP_SRC="contracts"
 export DAPP_LINK_TEST_LIBRARIES=0
-export DAPP_STANDARD_JSON="config.json"
+export DAPP_STANDARD_JSON="config-prod.json"
 
 dapp --use solc:0.6.11 build
