d03f734574616086e8798fae65001a6703d69b56
SatyamAgrawal <satyam0499@gmail.com>
2021-06-17 22:21:31 +0530

    ci test

--- test.sh
+++ test.sh
@@ -12,7 +12,7 @@ do
     esac
 done
 
-runs=$([ -z "$runs" ] && echo "10" || echo "$runs")
+runs=$([ -z "$runs" ] && echo "1" || echo "$runs")
 build=$([ -z "$build" ] && echo "1" || echo "$build")
 config=$([ -z "$config" ] && echo "config-dev.json" || echo "$config")
 skip_build=$([ "$build" == "0" ] && echo "1" || echo "0")
@@ -29,8 +29,8 @@ export DAPP_STANDARD_JSON=$config
 
 if [ "$skip_build" = "1" ]; then export DAPP_SKIP_BUILD=1; fi
 
-if [ -z "$test" ]; then match="[contracts/*/*/$version/test/*.t.sol]"; dapp_test_verbosity=1; else match=$test; dapp_test_verbosity=2; fi
+if [ -z "$test" ]; then match="contracts/core/pool/$version/test/PoolClaim.t.sol"; dapp_test_verbosity=1; else match=$test; dapp_test_verbosity=2; fi
 
-echo LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --cache "/home/admin/project/cache/dapp-cache" --fuzz-runs $runs
+echo LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --cache "temp-cache" --fuzz-runs $runs
 
-LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --cache "/home/admin/project/cache/dapp-cache" --fuzz-runs $runs
+LANG=C.UTF-8 dapp test --match "$match" --rpc-url "$ETH_RPC_URL" --verbosity $dapp_test_verbosity --cache "temp-cache" --fuzz-runs $runs
